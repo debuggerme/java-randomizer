4ed5ebe8c473ca22abd3a1013eb09019ed328da1
Jamith Nimantha <jamithnimantha@gmail.com>
2020-10-20 02:13:16 +0530

    Added Observer Design Pattern

---
+++ src/Observer/Publisher.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+/**
+ * @author Jamith Nimantha
+ */
+public class Publisher implements Subject {
+
+//    private List
+
+    public Publisher() {
+    }
+
+
+    @Override
+    public void registerObserver(Observer observer) {
+
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+
+    }
+
+    @Override
+    public void notifyObservers() {
+
+    }
+}
